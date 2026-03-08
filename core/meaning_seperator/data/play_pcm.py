0d22bfe6d5336bfce739aeb952e4905d6f687c7e
Ubuntu <ubuntu@ip-172-31-37-112.ap-northeast-2.compute.internal>
2026-03-08 04:33:20 +0000

    commit

---
+++ core/meaning_seperator/data/play_pcm.py
@@ -0,0 +1,10 @@
+# play_pcm.py
+import numpy as np
+import sounddevice as sd
+import sys
+
+path = sys.argv[1]  # 실행 시 파일 경로 인자로 받음
+audio = np.fromfile(f"{path}/eval_clean/KsponSpeech_E00001.pcm", dtype="<i2").astype("float32") / 32768.0
+print(f"길이: {len(audio)/16000:.2f}초")
+sd.play(audio, samplerate=16000)
+sd.wait()
