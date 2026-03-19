14a88fd3095072df81f3dad7bd0a4cf7e331be8a
DOODEOJI <jduh1020@naver.com>
2026-03-19 15:35:46 +0900

    파인튜닝 데이터 문장 끝 <SEG> 토큰 일괄 추가

--- Qwen3-ASR/finetuning/utils/convert_to_jsonl.py
+++ Qwen3-ASR/finetuning/utils/convert_to_jsonl.py
@@ -3,7 +3,7 @@ import os
 
 
 
-TRAIN_JSON = "/home/skkai/Desktop/STiTy/evaluation/KsponSpeech/transcribe/train.json"
+TRAIN_JSON = "/home/skkai/Desktop/STiTy/evaluation/KsponSpeech/results/train_gpt.json"
 AUDIO_DIR = "/home/skkai/Desktop/STiTy/evaluation/KsponSpeech/wav_data"
 OUTPUT_JSONL = "/home/skkai/Desktop/STiTy/Qwen3-ASR/finetuning/train.jsonl"
 LANGUAGE = "Korean"
@@ -12,14 +12,13 @@ with open(TRAIN_JSON, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 entries = data["data"]
-skipped = 0
 
 with open(OUTPUT_JSONL, "w", encoding="utf-8") as out:
     for entry in entries:
         audio_path = os.path.join(AUDIO_DIR, entry["file"] + ".wav")
         line = {
             "audio": audio_path,
-            "text": f"language {LANGUAGE}<asr_text>{entry['text']}"
+            "text": f"language {LANGUAGE}<asr_text>{entry['seg_text']}"
         }
         out.write(json.dumps(line, ensure_ascii=False) + "\n")
 
