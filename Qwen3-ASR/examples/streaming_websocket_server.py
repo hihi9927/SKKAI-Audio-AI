9d5a1bf0173dd79bbd88bd88c6bb84abfefd42f2
hyeseon04 <0147qkr@naver.com>
2026-05-06 07:57:04 +0900

    KtelSpeech 오류 수정

--- Qwen3-ASR/examples/streaming_websocket_server.py
+++ Qwen3-ASR/examples/streaming_websocket_server.py
@@ -1175,6 +1175,7 @@ class Qwen3ASRStreamingServer:
                     temperature=0.0,
                     max_tokens=self.config.max_new_tokens,
                     skip_special_tokens=False,  # DEBUG: special token 출력용
+                    repetition_penalty=1.3,
                 )
                 logger.info(f"Beam search enabled: beam_size={self.config.beam_size}")
             except TypeError:
